263651b26aa29ef69d43a0eb35b96f0a87c61840
haris <haresh003@e.ntu.edu.sg>
2022-07-07 20:34:06 +0800

    finished inital extract pipeline

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,5 +23,29 @@ tags = {
   Name= "inital_etl"
   Environment = "Dev"
 }
+}
   
+resource "aws_s3_bucket" "load_bucket" {
+bucket = "initial-data-load-bucket"
+
+tags = {
+  Name= "inital_load"
+  Environment = "Dev"
+}
+}
+resource "aws_s3_bucket" "comm_bucket" {
+bucket = "initial-comments-load-bucket"
+
+tags = {
+  Name= "inital_load"
+  Environment = "Dev"
+}
+}
+resource "aws_s3_bucket" "comm_trans_bucket" {
+bucket = "comments-transformed-bucket"
+
+tags = {
+  Name= "with_transformation"
+  Environment = "Dev"
+}
 }
